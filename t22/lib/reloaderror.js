ac891160d41e5a3cf5da4741bba768c933760e98
김지훈 <softm@nate.com>
2023-10-05 18:29:48 +0900

    ~

--- t22/lib/reloaderror.js
+++ t22/lib/reloaderror.js
@@ -12,7 +12,7 @@ var reloadLimit = 5;
 if ( reloadTimes.length >= reloadLimit ) {
     var intervalSec = (reloadTimes[reloadTimes.length-1] - reloadTimes[reloadTimes.length-reloadLimit]) / 1000;
     console.log("intervalSec", intervalSec, reloadTimes.length, reloadTimes.length-reloadLimit);
-    if ( intervalSec <= (reloadLimit+1) ) {
+    if ( intervalSec <= (reloadLimit+5) ) {
         fn_DeviceSaveData("_NAVI_POPUP_MODE", "0");
         AlertUtil.fn_Alert("실행중 문제가 발행하여, 안내아이콘 설정을 초기화였습니다.업데이트 완료후 다시 설정해주세요.");
         console.log("intervalSec", reloadLimit.length-reloadLimit,"~" ,reloadLimit.length-1);
