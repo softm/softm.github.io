2758dcc00c48850b658a7e1467381cadd5a46fd3
김지훈 <softm@nate.com>
2024-12-13 20:38:50 +0900

    t54 - recover

---
+++ t54/lib/recover.js
@@ -0,0 +1,10 @@
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+
+Storage.prototype.getObject = function(key) {
+    return JSON.parse(this.getItem(key));
+}
+
+
+fn_DeviceSaveData("_NAVI_POPUP_MODE", "0");
