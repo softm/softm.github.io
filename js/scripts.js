29c0f7e6e94949fcfaaacb6d8d45f39d29828e78
김지훈 <softm@nate.com>
2025-01-01 02:39:16 +0900

    ~

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
+* 티맵자동실행 - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 티맵자동실행
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
